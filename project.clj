505fd8612eb2c0d9d4b5b505b2f95d19fe22ef4e
Phill Wolf <phill.wolf@gmail.com>
2013-10-19 22:24:40 -0400

    Release 0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.hoofdust/xml-skim "0.2-SNAPSHOT"
+(defproject com.hoofdust/xml-skim "0.2"
   :description "Skims data structures from XML using streaming APIs"
   :url "http://hoofdust.com/"
   :license {:name "Eclipse Public License"
